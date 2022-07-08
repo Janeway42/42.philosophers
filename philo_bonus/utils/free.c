b551e843ee3c03abbf5f082e50b6242982a9f23b
Janeway42 <73645395+Janeway42@users.noreply.github.com>
2022-07-08 11:57:36 +0200

    Update free.c

--- philo_bonus/utils/free.c
+++ philo_bonus/utils/free.c
@@ -16,6 +16,10 @@ void	free_stuff(t_data *data)
 {
 	if (data->philos)
 		free(data->philos);
+	// if (data->s_forks)
+	//	free(data->s_forks);
+	// if (data->s_write)
+	//	free(data->s_write);
 	if (data->process_id)
 		free(data->process_id);
 }
